12cbaf1c1efabc518e148083bc72ad499853cba3
kylee <kylee@soundl.ly>
2016-03-11 15:21:17 +0900

    안드로이드 프로젝트 생성

---
+++ Android/app/src/main/java/com/soundlly/enjoylaunchforsalaryman/activities/AddLaunchActivity.java
@@ -0,0 +1,17 @@
+package com.soundlly.enjoylaunchforsalaryman.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by soundllydev on 2016. 3. 11..
+ */
+public class AddLaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
