c84bfc69b768cc5075962f14172e8002c28d44b2
kylee <kylee@soundl.ly>
2016-03-11 17:12:09 +0900

    점심 추가 액티비티 생성

--- Android/app/src/main/java/com/soundlly/enjoylaunchforsalaryman/activities/AddLaunchActivity.java
+++ Android/app/src/main/java/com/soundlly/enjoylaunchforsalaryman/activities/AddLaunchActivity.java
@@ -3,6 +3,10 @@ package com.soundlly.enjoylaunchforsalaryman.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.soundlly.enjoylaunchforsalaryman.R;
 
 /**
  * Created by soundllydev on 2016. 3. 11..
@@ -12,6 +16,21 @@ public class AddLaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_launch);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                supportFinishAfterTransition();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
